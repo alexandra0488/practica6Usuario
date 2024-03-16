6cb67be282eb2f6a75bbade285d0462c0bd0e718
Alexandra R <alexandra.rodriguez.ramos88@gmail.com>
2024-03-16 18:55:38 +0100

    componente card

---
+++ src/app/components/usuarios-card/usuarios-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { IUsuarios } from '../../interfaces/iusuarios.interface';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-usuarios-card',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './usuarios-card.component.html',
+  styleUrl: './usuarios-card.component.css'
+})
+export class UsuariosCardComponent {
+
+  @Input() usuario!: IUsuarios;
+
+ 
+}
