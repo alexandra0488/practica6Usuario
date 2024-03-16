98bdd093d53a50346bed38637fd8e9b66fa293de
Alexandra R <alexandra.rodriguez.ramos88@gmail.com>
2024-03-16 18:51:20 +0100

    incorporacion del enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { UsuarioListComponent } from './pages/usuario-list/usuario-list.component';
+import { UsuarioViewComponent } from './pages/usuario-view/usuario-view.component';
+import { UsuarioFormComponent } from './pages/usuario-form/usuario-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:"", pathMatch: 'full', redirectTo: "home"},
+    {path: 'home', component: UsuarioListComponent} ,
+    {path: 'user/:id', component: UsuarioViewComponent} ,
+
+   
+    {path: 'update/:id', component: UsuarioFormComponent},
+
+    {path:'newuser', component:UsuarioFormComponent},
+   
+
+
+];
